5497474971cd65bb9f0ab10956e16fdc20726f8b
Rebekah-codes <B3cksta@hotmail.co.uk>
2025-10-02 14:52:32 +0100

    Homepage egg loop, hatch view, route

--- core/urls.py
+++ core/urls.py
@@ -3,4 +3,5 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('hatch/<int:egg_id>/', views.hatch_egg, name='hatch_egg'),
 ]
--- core/views.py
+++ core/views.py
@@ -1,4 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Egg
 
+# Homepage: show all eggs
+# Optionally filter: eggs = Egg.objects.filter(is_hatched=False)
 def home(request):
-    return render(request, 'home.html')
+    eggs = Egg.objects.all()
+    return render(request, 'home.html', {'eggs': eggs})
+
+# Hatch egg view
+def hatch_egg(request, egg_id):
+    egg = get_object_or_404(Egg, id=egg_id)
+    if request.method == 'POST' and not egg.is_hatched:
+        egg.is_hatched = True
+        egg.save()
+        # Optionally: create Dinosaur instance here
+    return redirect('home')
