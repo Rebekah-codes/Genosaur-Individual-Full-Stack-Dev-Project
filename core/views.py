e3dfc6834b6c59d5287560c72b216b84dc0a9c36
Rebekah-codes <B3cksta@hotmail.co.uk>
2025-10-06 14:25:43 +0100

    Fix: add missing import for login_required in views.py

--- core/views.py
+++ core/views.py
@@ -32,6 +32,7 @@ def your_dinosaurs(request):
 # Imports
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.decorators import login_required
 from django.contrib.auth import logout as auth_logout, login as auth_login
 from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 from django.utils.crypto import get_random_string
